cdef1254360ba9000ea01fc8142c168315a994dc
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-07-23 22:27:14 -0400

    error fix

--- react-website-1/src/components/nav/Nav.jsx
+++ react-website-1/src/components/nav/Nav.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import './nav.css';
-import {BiHomeAlt} from 'react-icons/bi';
-import {BiUser} from 'react-icons/bi';
-import {BiBookAlt} from 'react-icons/bi';
-import {RiServiceLine} from 'react-icons/ri';
-import {BiMessageSquareDetail} from 'react-icons/bi';
-import {useState} from 'react';
+import React from 'react'
+import './nav.css'
+import {BiHomeAlt} from 'react-icons/bi'
+import {BiUser} from 'react-icons/bi'
+import {BiBookAlt} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
 <a href="#" className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
-<a href='#about' onClick={() => setActiveNave('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser/></a>
+<a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser/></a>
 <a href='#experience'><BiBookAlt/></a>
 <a href='#services'><RiServiceLine/></a>
 <a href='#contact'><BiMessageSquareDetail/></a>
