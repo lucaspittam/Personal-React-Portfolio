0d16e9abe2dc8f53910a19fb7f838123e513d761
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-07-26 11:50:56 -0400

    Update about section

--- react-website-1/src/components/about/About.jsx
+++ react-website-1/src/components/about/About.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 const About = () => {
   return (
     <section id='about'>
-      
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='about_container'>
+        <div className='about_me'>
+            <div className="about_me-image">
+              <img src={ME} alt="about "/>
+            </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small> 3+ Years Working</small>
+            </article>
+
+            <article className='about_card'>
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small> 50+ Globally</small>
+            </article>
+
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Projects</h5>
+              <small> 100+ Completed!</small>
+            </article>
+
+          </div>
+
+
+        </div>
+
+      </div>
     </section>
   )
 }
